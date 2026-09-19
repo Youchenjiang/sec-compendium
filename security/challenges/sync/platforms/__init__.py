1c0e3db29379eebbf7b8209b797587bf310041e0
Youchen Jiang <1014308g@gmail.com>
2026-09-19 22:16:20 +0800

    feat(challenge): register PortSwigger and CyLab adapters in registry

--- security/challenges/sync/platforms/__init__.py
+++ security/challenges/sync/platforms/__init__.py
@@ -3,11 +3,15 @@ Platform Adapters Registry
 """
 
 from security.challenges.sync.platforms.cyberdefenders import CyberDefendersAdapter
+from security.challenges.sync.platforms.cylab_picoctf import CyLabAdapter
 from security.challenges.sync.platforms.hackthebox import HackTheBoxAdapter
 from security.challenges.sync.platforms.mta import MTAAdapter
+from security.challenges.sync.platforms.portswigger import PortSwiggerAdapter
 
 ALL_ADAPTERS = {
     "cyberdefenders": CyberDefendersAdapter,
     "hackthebox": HackTheBoxAdapter,
     "mta": MTAAdapter,
+    "portswigger": PortSwiggerAdapter,
+    "cylab_picoctf": CyLabAdapter,
 }
